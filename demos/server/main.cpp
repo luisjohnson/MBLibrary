1f0489afab97dbfc881c18d53848ea903fd3456d
Luis Johnson <luisaljohnson@gmail.com>
2024-04-02 14:51:55 -0700

    Increase data area generation and adjust output frequency
    The number of datapoints generated for Coils, Discrete Inputs, Input Registers, and Holding Registers has been doubled from 8 to 16. Meanwhile, the frequency of output has been switched from every 5 seconds to every 500 milliseconds to accommodate for the increased data production. The console clearing command has been commented out to prevent abrupt screen wipeouts.

--- demos/server/main.cpp
+++ demos/server/main.cpp
@@ -8,25 +8,27 @@
 int main() {
     Modbus::DataArea dataArea;
 
-    dataArea.generateCoils(0, 8, Modbus::ValueGenerationType::Random);
-    dataArea.generateDiscreteInputs(0, 8, Modbus::ValueGenerationType::Random);
-    dataArea.generateInputRegisters(0, 8, Modbus::ValueGenerationType::Random);
-    dataArea.generateHoldingRegisters(0, 8, Modbus::ValueGenerationType::Random);
+    dataArea.generateCoils(0, 16, Modbus::ValueGenerationType::Random);
+    dataArea.generateDiscreteInputs(0, 16, Modbus::ValueGenerationType::Random);
+    dataArea.generateInputRegisters(0, 16, Modbus::ValueGenerationType::Random);
+    dataArea.generateHoldingRegisters(0, 16, Modbus::ValueGenerationType::Random);
 
     std::thread printer([&dataArea]() {
+
         for (;;) {
-            std::system("clear");
-            std::cout << std::left << std::setw(10) << "Address" << std::setw(10) << "Coils" << std::setw(20) << "Discrete Inputs" << std::setw(20)
+//            std::system("cls");
+            std::cout << std::left << std::setw(10) << "Address" << std::setw(10) << "Coils" << std::setw(20)
+                      << "Discrete Inputs" << std::setw(20)
                       << "Holding Registers" << std::setw(20) << "Input Registers" << std::endl;
-            for (int i = 0; i < 8; i++) {
-                std::cout << std::setw(10) << i << std::setw(10) << (dataArea.getCoils(i, 1)[0].read() ? "1" : "0") << std::setw(20)
+            for (int i = 0; i < 16; i++) {
+                std::cout << std::setw(10) << i << std::setw(10) << (dataArea.getCoils(i, 1)[0].read() ? "1" : "0")
+                          << std::setw(20)
                           << (dataArea.getDiscreteInputs(i, 1)[0].read() ? "1" : "0") << std::setw(20)
                           << dataArea.getHoldingRegisters(i, 1)[0].read() << std::setw(20)
                           << dataArea.getInputRegisters(i, 1)[0].read() << std::endl;
+//                std::cout << std::endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
             }
-
-            std::cout << std::endl;
-            std::this_thread::sleep_for(std::chrono::seconds(5));
         }
     });
 
